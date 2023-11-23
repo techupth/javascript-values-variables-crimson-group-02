041bedbbcce00a37fd3c37b1a7b655954ec92857
kaew <rajitkaew2285@gmail.com>
2023-11-23 13:29:05 +0700

    feat(ex1and2):completed exercise

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,14 @@
-//Exercise #1: Variable Creation
-//Start coding here
+let personName = "Karter";
+let personLastName = "Anderson";
+let addressHome = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const nameRestaurant = "Chef's Table by Chef James";
+const billFoodPrice = 1500;
+const numberRestaurant = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+addressHome = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641 ";
+billFoodPrice = 5000;
+numberRestaurant = 2500;
